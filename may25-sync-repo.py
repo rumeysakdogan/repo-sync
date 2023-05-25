8415b909f870a997f0613237e293554a066b7288
rumeysakdogan <dgn.rumeysaa@gmail.com>
2023-05-25 14:48:20 -0400

    update may25-sync-repo.py

--- may25-sync-repo.py
+++ may25-sync-repo.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import shutil
 from github import Github
 from git import Repo
@@ -6,8 +7,6 @@ import json
 from concurrent.futures import ThreadPoolExecutor
 import logging
 import time
-import requests
-from requests.auth import HTTPBasicAuth
 
 SOURCE_URL = "https://github.com"
 DESTINATION_URL = "https://dev.azure.com"
@@ -22,31 +21,19 @@ LOCAL_PATH = os.environ['RUNNER_TEMP']
 
 logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.INFO)
 
-
 def create_ado_repository(org_name: str, project_name: str, repo_name: str) -> None:
-    url = f"https://dev.azure.com/{org_name}/{project_name}/_apis/git/repositories?api-version=6.0"
-    data = {"name": repo_name}
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(
-        url,
-        data=json.dumps(data),
-        headers=headers,
-        auth=HTTPBasicAuth("", DESTINATION_PERSONAL_ACCESS_TOKEN),
-    )
-    response.raise_for_status()
+    cmd = ['az', 'repos', 'create', '--org',
+           f'https://dev.azure.com/{org_name}', '--project', project_name, '--name', repo_name, '--output','table']
+    subprocess.run(cmd, check=True)
 
 
 def list_ado_repositories(org_name: str, project_name: str) -> list:
     destination_repos = []
-    url = f"https://dev.azure.com/{org_name}/{project_name}/_apis/git/repositories?api-version=6.0"
-    headers = {"Content-Type": "application/json"}
-    response = requests.get(
-        url, headers=headers, auth=HTTPBasicAuth("", DESTINATION_PERSONAL_ACCESS_TOKEN)
-    )
-    response.raise_for_status()
-    repos = json.loads(response.text)["value"]
+    cmd = f'az repos list --org https://dev.azure.com/{org_name} --project {project_name}'
+    output = os.popen(cmd).read()
+    repos = json.loads(output)
     for repo in repos:
-        destination_repos.append(repo["name"])
+        destination_repos.append(repo['name'])
     return destination_repos
 
 
@@ -56,82 +43,95 @@ def get_default_branch(repo_name: str) -> str:
     return repo.default_branch
 
 
-def clone_and_push(repo_name: str) -> None:
-    logging.info(f'Cloning repo: {repo_name} started.')
-    default_branch = get_default_branch(repo_name)
-
-    # Clone only the latest version of the default branch from Github
-    clone_url = f"{SOURCE_URL}/{SOURCE_USER}/{repo_name}.git"
-    repo_path = f"/tmp/{repo_name}"
-
-    # Check if repo_path already exists and is not empty
-    if os.path.exists(repo_path) and os.listdir(repo_path):
-        # Delete the existing directory and its contents
-        shutil.rmtree(repo_path)
-
-    repo = Repo.clone_from(clone_url, repo_path, branch=default_branch, depth=1)
-
-    # Remove history and initialize new repository
-    shutil.rmtree(os.path.join(repo_path, ".git"))
-    tmp_repo = Repo.init(repo_path)
-
-    # Make an initial commit
-    tmp_repo.git.add(A=True)
-    tmp_repo.index.commit("Update from GitHub")
-
-    # Add destination remote
-    remote_name = "destination"
-    remote_url = f"{DESTINATION_URL}/{DESTINATION_ORG}/{DESTINATION_PROJECT}/_git/{repo_name}"
-    remote_url_with_token = remote_url.replace(
-        "https://", f"https://{DESTINATION_PERSONAL_ACCESS_TOKEN}@"
-    )
-    tmp_repo.create_remote(remote_name, url=remote_url_with_token)
-
-    # Push changes to destination
-    tmp_repo.remotes.destination.push(f"HEAD:refs/heads/{default_branch}", force=True)
-
-    # Clean up
-    shutil.rmtree(repo_path)
-    logging.info(f'Cloning repo: {repo_name} complete.')
-
-
-def synchronize_and_push(repo_name: str) -> None:
-    logging.info(f'Synchronization of repo: {repo_name} started.')
-    default_branch = get_default_branch(repo_name)
-
-    # Set up source and destination repository URLs
-    source_clone_url = f"{SOURCE_URL}/{SOURCE_USER}/{repo_name}.git"
-    destination_clone_url = (
-        f"{DESTINATION_URL}/{DESTINATION_ORG}/{DESTINATION_PROJECT}/_git/{repo_name}"
-    )
-    destination_clone_url_with_token = destination_clone_url.replace(
-        "https://", f"https://{DESTINATION_PERSONAL_ACCESS_TOKEN}@"
-    )
-
-    # Set up temporary repository path
-    tmp_repo_path = f"/tmp/{repo_name}"
-    if os.path.exists(tmp_repo_path):
-        shutil.rmtree(tmp_repo_path)
-
-    # Clone source repository
-    Repo.clone_from(source_clone_url, tmp_repo_path, branch=default_branch, depth=1)
-
-    # Add destination remote
-    tmp_repo = Repo(tmp_repo_path)
-    tmp_repo.create_remote("destination", url=destination_clone_url_with_token)
-
-    # Pull changes from destination repository
+def clone_and_push(repo_name: str) -> tuple:
     try:
-        tmp_repo.remotes.destination.pull(default_branch)
+        logging.info(f'Cloning repo: {repo_name} started.')
+        default_branch = get_default_branch(repo_name)
+
+        # Clone only the latest version of the default branch from Github
+        clone_url = f'{SOURCE_URL}/{SOURCE_USER}/{repo_name}.git'
+        repo_path = f'{LOCAL_PATH}/tempdir/{repo_name}'
+
+        # Check if repo_path already exists and is not empty
+        if os.path.exists(repo_path) and os.listdir(repo_path):
+            # Delete the existing directory and its contents
+            shutil.rmtree(repo_path)
+
+        repo = Repo.clone_from(clone_url, repo_path,
+                            branch=default_branch, depth=1)
+
+        # Remove history and initi
+        os.system(f'cd {repo_path} && rm -rf .git && git init')
+
+        # Make an initial commit
+        repo.index.add("*")
+
+        hidden_files = [file for file in repo.untracked_files if file.startswith('.')]
+        if hidden_files:
+            repo.index.add(hidden_files)
+        else:
+            print("No hidden files found")
+        repo.index.commit("Initial commit")
+
+        # Push to destination
+        remote_name = 'destination'
+        remote_url = f'{DESTINATION_URL}/{DESTINATION_ORG}/{DESTINATION_PROJECT}/_git/{repo_name}'
+        remote_url_with_token = remote_url.replace(
+            'https://', f'https://{DESTINATION_PERSONAL_ACCESS_TOKEN}@')
+        repo.create_remote(remote_name, url=remote_url_with_token)
+        repo.git.push(remote_name, f'HEAD:refs/heads/{default_branch}', force=True)
+
+        # Clean up
+        os.system(f'rm -rf {repo_path}')
+        logging.info(f'Cloning repo: {repo_name} complete.')
+        return (repo_name, None)
     except Exception as e:
-        logging.warning(f"Failed to pull changes from destination repository: {e}")
+        return (repo_name, e)
 
-    # Push changes to destination
-    tmp_repo.remotes.destination.push(f"HEAD:refs/heads/{default_branch}", force=True)
 
-    # Clean up
-    shutil.rmtree(tmp_repo_path)
-    logging.info(f'Synchronization of repo: {repo_name} complete.')
+def synchronize_and_push(repo_name: str) -> tuple:
+    try:
+        logging.info(f'Synchronization of repo: {repo_name} started.')
+        default_branch = get_default_branch(repo_name)
+
+        # Set up source and destination repository URLs
+        source_clone_url = f"{SOURCE_URL}/{SOURCE_USER}/{repo_name}.git"
+        destination_clone_url = (
+            f"{DESTINATION_URL}/{DESTINATION_ORG}/{DESTINATION_PROJECT}/_git/{repo_name}"
+        )
+        destination_clone_url_with_token = destination_clone_url.replace(
+            "https://", f"https://{DESTINATION_PERSONAL_ACCESS_TOKEN}@"
+        )
+
+        # Set up temporary repository path
+        tmp_repo_path = f"{LOCAL_PATH}/tempdir/{repo_name}"
+        if os.path.exists(tmp_repo_path):
+            shutil.rmtree(tmp_repo_path)
+
+        # Clone source repository
+        Repo.clone_from(source_clone_url, tmp_repo_path, branch=default_branch, depth=1)
+
+        # Remove history and initialize new repository
+        shutil.rmtree(os.path.join(tmp_repo_path, ".git"))
+        tmp_repo = Repo.init(tmp_repo_path)
+
+        # Make an initial commit
+        tmp_repo.git.add(A=True)
+        tmp_repo.index.commit("Update from GitHub")
+
+        # Add destination remote
+        tmp_repo.create_remote("destination", url=destination_clone_url_with_token)
+
+        # Push changes to destination
+        tmp_repo.remotes.destination.push(f"HEAD:refs/heads/{default_branch}", force=True)
+
+        # Clean up
+        shutil.rmtree(tmp_repo_path)
+        logging.info(f'Synchronization of repo: {repo_name} complete.')
+        return (repo_name, None)
+    except Exception as e:
+        return (repo_name, e)
+
 
 def main():
     start_time = time.time()
@@ -143,8 +143,7 @@ def main():
     source_repos = g.get_user().get_repos()
     source_repos = [
         repo for repo in source_repos if not repo.name.startswith(RESTRICTED_PREFIX)
-        and not repo.name.startswith(ASSET_PREFIX)
-        and repo.name.startswith("gh")
+        and repo.name.startswith("skill")
     ]
     logging.info(f"{len(source_repos)} repositories found in GitHub.")
 
@@ -166,12 +165,13 @@ def main():
             for repo in new_repos
         ]
         futures += [executor.submit(clone_and_push, repo.name) for repo in new_repos]
-
+        
         for future in futures:
-            try:
-                future.result()
-            except Exception as e:
-                print(f"Error: Task failed with exception: {e}")
+            result = future.result()
+            if result is not None:
+                repo_name, error = result
+                if error:
+                    print(f"Error: Task failed with exception: {error} for repository: {repo_name}")
 
     # Pull latest changes from main/master branch of existing repositories and push to destination platform
     existing_repos = [
@@ -183,12 +183,13 @@ def main():
         futures = [
             executor.submit(synchronize_and_push, repo.name) for repo in existing_repos
         ]
-
         for future in futures:
-            try:
-                future.result()
-            except Exception as e:
-                print(f"Error: Task failed with exception: {e}")
+            result = future.result()
+            if result is not None:
+                repo_name, error = result
+                if error:
+                    print(f"Error: Task failed with exception: {error} for repository: {repo_name}")
+  
     exec_time = time.gmtime(time.time() - start_time)
 
     logging.info(
